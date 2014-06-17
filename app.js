b5d608fb8f56c9a38182d86213a388b70061c77a
Justin Martinez <jlmart88@mit.edu>
2014-06-17 14:31:01 +0200

    Added signature checking in app.js, will remove in geo.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var crypto = require('crypto');
 
 //Set up MongoDB dependencies
 // Database
@@ -19,6 +20,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+app.set('env','development');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -27,6 +29,8 @@ app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
 // Make db accessible to router
 app.use(function(req,res,next){
     req.db = db;
@@ -34,6 +38,18 @@ app.use(function(req,res,next){
 });
 
 app.use('/', routes);
+
+//verify the signature if it is a geo request
+app.use(function(req,res,next){
+    if (req.path.indexOf('/geo')>-1){
+        verifySignature(req,function(err,verify){
+            if (verify){
+                next();
+            } else unauthorizedErrorHandler(res);
+        });
+    } else next();
+});
+
 app.use('/geo', geo);
 
 
@@ -50,6 +66,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
+        console.error(err);
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
@@ -68,5 +85,48 @@ app.use(function(err, req, res, next) {
     });
 });
 
+// Verify the signature header attached to a request
+function verifySignature(req, callback){
+    var db = req.db;
+    var body;
+    //console.log(req); 
+    if (req.method == 'POST' || req.method == 'DELETE'){
+        body = req.body;
+    } else body = req.query;
+    db.collection('clientidlist').findOne({'clientID':body.clientID}, function(err, result){
+        if (err!=null){
+            databaseResultHandler(res,err,result);
+        }
+        if (result!=null){
+            console.log("Server-side String:");
+            console.log(JSON.stringify(body)+req.headers['x-timestamp']+result.clientSecret);
+            
+            var signature = crypto.createHash(req.headers['x-authentication-type']).update(
+                        JSON.stringify(body)+req.headers['x-timestamp']+result.clientSecret).digest('hex');
+            console.log("Server-side Hash:");
+            console.log(signature);
+            console.log("Received Signature:");
+            console.log(req.headers['x-signature']);
+            console.log("Received Time:");
+            console.log(parseFloat(req.headers['x-timestamp'])*1000);
+            console.log("Current Server Time:");
+            console.log((new Date).getTime());
+            console.log("Time Difference:");
+            console.log((new Date).getTime() - parseFloat(req.headers['x-timestamp'])*1000);    
+           //Check that the signature is correct and the request is less than 5 minutes old
+            var expireTime = 1000*60*5;
+            callback(null,(signature == req.headers['x-signature'] && 
+                (new Date).getTime() - parseFloat(req.headers['x-timestamp'])*1000 < expireTime));
+        }
+        else {
+            callback(null,false);
+        }
+    });
+}
+
+// Send an unauthroized request error to the client
+function unauthorizedErrorHandler(res){
+    res.send({error:201, cause:"Unauthorized request caught in app.js"});
+}
 
 module.exports = app;
